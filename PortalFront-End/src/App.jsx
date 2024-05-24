1181a9bbe41e816238a30748f10c342fb2e2e1b2
Tejasshinde-123 <tejasrshinde2000@gmail.com>
2024-05-24 11:19:36 +0530

    Your descriptive commit message here

--- PortalFront-End/src/App.jsx
+++ PortalFront-End/src/App.jsx
@@ -3,15 +3,19 @@ import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage/HomePage";
 import Dashboard from "./pages/AdminPages/Dashboard";
 import StudentsList from "./pages/AdminPages/Students/StudentsList";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Trainer from "./pages/AdminPages/Trainer/Trainer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/admin/dashboard" element={<Dashboard />} />
           <Route path="/admin/students" element={<StudentsList />} />
+          <Route path="/admin/Trainer" element={< Trainer/>} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ PortalFront-End/src/pages/AdminPages/Trainer/Trainer.jsx
@@ -0,0 +1,11 @@
+import Layout from "../../../components/Layout";
+
+function Trainer() {
+  return (
+    <Layout>
+    <div>TrainerList</div>
+  </Layout>
+  )
+}
+
+export default Trainer
